83f75e783f40b72878c8662660ee857867c55d96
JrSchild <joriami@gmail.com>
2015-06-01 20:55:16 +0200

    Working images from Flickr in collectionview

---
+++ Glimp/Glimp/FlickrPhotoCell.swift
@@ -0,0 +1,14 @@
+//
+//  FlickrPhotoCell.swift
+//  Glimp
+//
+//  Created by Joram Ruitenschild on 01-06-15.
+//  Copyright (c) 2015 Joram Ruitenschild. All rights reserved.
+//
+
+import UIKit
+
+class FlickrPhotoCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+}
